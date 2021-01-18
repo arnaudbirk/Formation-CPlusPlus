e26f39f747515ae4027c94f9c3585faa247936cc
Arnaud Birk <arnaud.birk@ign.fr>
2021-01-18 10:53:31 +0100

    Ajout Exercice5

--- Exercices/Exercice5/Exercice5.cpp
+++ Exercices/Exercice5/Exercice5.cpp
@@ -1,4 +1,4 @@
-#include<iostream>
+/*#include<iostream>
 using namespace std;
 
 void swap_pointeur(int *pa, int *pb)
@@ -37,5 +37,43 @@ int main()
     cout << "Apres swap par pointeur : " << a << " " << b << endl;
     swap_pointeur(&a,&b);
     cout << "Apres swap par reference : " << a << " " << b << endl;
+    return 0;
+}*/
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+void sauvegardeTableau(string tab[] , int taille){
+    ofstream fout("tab.txt"); //on crée un flux de sortie fout vers le fichier tab.txt
+    if(!fout){
+        cout << "Pb à l'ouverture";
+        return;
+    }
+    fout << taille << endl;
+    for(int i=0; i<taille; i++){
+        fout << tab[i] << endl;
+    }
+}
+
+void lireFichier(){
+    ifstream fin("tab.txt");
+    string ligne;
+
+    while(getline(fin,ligne)){
+      std::cout << ligne << std::endl;
+    }
+}
+
+int main()
+{
+    string tab[3] = {"titi", "tutu", "toto"};
+
+    sauvegardeTableau(tab, 3);
+    lireFichier();
+
     return 0;
 }
---
+++ Exercices/Exercice5/README.md
@@ -0,0 +1,7 @@
+# Exercice 5
+
+* Faites une fonction qui permet de sauvegarder un tableau dans un fichier texte qui écrit sur la première ligne la taille du tableau puis un élément du tableau par ligne.
+
+* Créez la fonction qui permet de lire le fichier précédemment créé.
+
+[Lien vers la correction](https://repl.it/@arnaudbirk/Exercice5#main.cpp)
